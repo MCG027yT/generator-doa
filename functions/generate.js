460e8695b9347cd1fe78403a6293fe49dc204a09
PvTzOnGH <72125799+MCG027yT@users.noreply.github.com>
2025-10-07 19:34:12 +0700

    Update generate.js

--- functions/generate.js
+++ functions/generate.js
@@ -106,7 +106,7 @@ const tags = await runCFText(CF_ACCOUNT_ID, CF_TOKEN, MODEL_TXT,
 );
 
 const arab = await runCFText(CF_ACCOUNT_ID, CF_TOKEN, MODEL_TXT,
-  `buatkan "${kata}". dalam bahasa arab lengkap dengan harkat, hanya menampilkan tulisan arab tanpa penjelasan apapun.`
+  `buatkan "${kata}". secara singkat dalam bahasa arab lengkap dengan harkat, hanya menampilkan tulisan arab tanpa penjelasan apapun.`
 );
 
 const indo = await runCFText(CF_ACCOUNT_ID, CF_TOKEN, MODEL_TXT,
